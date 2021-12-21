137e5659d2021c483dc2398eaebc11d78bf24cbf
rhqudco <rhqudco1204@naver.com>
2021-12-22 00:25:58 +0900

    fade Effect Failed....

--- js/about.js
+++ js/about.js
@@ -1,14 +1,20 @@
 $(window).scroll(function () {
-	let scrollValue = $(document).scrollTop();
+	var scrollValue = $(document).scrollTop();
+    var weAre = $('#WeAre');
 
 	console.log(scrollValue);
-    
-    if(scrollValue > 500) { // 786
-        $('#WeAre').fadeTo(1000, 0.0);
-    }
+    console.log();
 
-    if(scrollValue > 1500) {
-        $('#Major').fadeTo(1000, 0.0);
-    }
-
+        // if(scrollValue > 500) { // 786
+        //     $('#WeAre').fadeTo(1000, 0.0);
+        // }
+        // // if(scrollValue < 499) {
+        // //     $('#WeAre').fadeTo(1000, 1.0);
+        // // }
+        // if(scrollValue > 1400) {
+        //     $('#Major').fadeTo(1000, 0.0);
+        // }
+        // // if(scrollValue < 1399) {
+        // //     $('#Major').fadeTo(1000, 1.0);
+        // // }
 });
