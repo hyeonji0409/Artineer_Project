751303efc5eb72b0bacad1e9ed226b870ea269d3
hyeonji0409 <guswl8507@naver.com>
2021-12-23 10:15:25 +0900

    help...

--- js/login.js
+++ js/login.js
@@ -2,3 +2,11 @@ function openLogin() {
     window.open("login.html", "login",
         "top=200, left=500, width=400, height=450, directories='no', location=no, menubar=no, resizable=yes, status=yes, toolbar=no, scrollbar=no");
 }
+
+function check() {
+    let userid = 'root';
+    let userpw = 'root';
+
+    let inputid = document.getElementById('id_input');
+    let inputpw = document.getElementById('pw_input');
+}
