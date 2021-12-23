fe5627d0192f7d680144ddeb8d5f88084802dbfa
hyeonji0409 <guswl8507@naver.com>
2021-12-23 14:57:49 +0900

    index, login

--- js/login.js
+++ js/login.js
@@ -1,12 +1,39 @@
-function openLogin() {
-    window.open("login.html", "login",
-        "top=200, left=500, width=400, height=450, directories='no', location=no, menubar=no, resizable=yes, status=yes, toolbar=no, scrollbar=no");
-}
 
-function check() {
-    let userid = 'root';
-    let userpw = 'root';
+$(function () {
+    // 로그인 버튼 누르면 로그인 창 띄우기
+    $('#index_login').on('click', function () {
+        window.open("login.html", "login",
+            "top=200, left=500, width=400, height=450, directories='no', location=no, menubar=no, resizable=yes, status=yes, toolbar=no, scrollbar=no");
+    });
 
-    let inputid = document.getElementById('id_input');
-    let inputpw = document.getElementById('pw_input');
-}
+    $('.sign_in_button').on('click', function () {
+        let inputid = document.getElementById('id_input');
+        let inputpw = document.getElementById('pwd_input');
+
+        let userid = 'root';
+        let userpw = '1234';
+
+        if (inputid.value == userid && inputpw.value == userpw) {
+            alert("로그인 성공");
+            window.close();
+        }
+        else
+            alert("아이디 혹은 비밀번호가 잘못 입력되었습니다.");
+    });
+
+});
+
+function onKeyupPasswd(ev) {
+    var evKeyup = null;
+
+    if (ev)
+        evKeyup = ev;
+    else
+        evKeyup = window.event;
+
+    if (evKeyup.keyCode == 13) { // enter key code:13
+        document.getElementById("pwd_input").blur();
+        check();
+    }    // end if
+
+}    // end function onKeyupPasswd()
