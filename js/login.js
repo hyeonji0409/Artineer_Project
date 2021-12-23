762e4e967b3f4764f499140060f5ed2e85ce1af8
rhqudco <rhqudco1204@naver.com>
2021-12-23 12:00:04 +0900

    login fix....

--- js/login.js
+++ js/login.js
@@ -2,3 +2,8 @@ function openLogin() {
     window.open("login.html", "login",
         "top=200, left=500, width=400, height=450, directories='no', location=no, menubar=no, resizable=yes, status=yes, toolbar=no, scrollbar=no");
 }
+let joinButton = document.getElementById('join');
+joinButton.onclick(showPopUp());
+function showPopUp() {
+    window.open("join.html", "회원가입", "width=800, height=800, left=100, top=50");
+}
